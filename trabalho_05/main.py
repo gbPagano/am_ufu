81a9b892126b6bdd901043683517d5187a007b0f
gbPagano <guilhermebpagano@gmail.com>
2023-04-06 21:47:16 -0300

    trabalho5

--- trabalho_05/main.py
+++ trabalho_05/main.py
@@ -1,217 +1,29 @@
-# Número -1
-numero_zero = [
-  -1, 1, 1, 1, -1,
-  1, -1, -1, -1, 1,
-  1, -1, -1, -1, 1,
-  1, -1, -1, -1, 1,
-  -1, 1, 1, 1, -1
-]
-
-# Número 1
-numero_um = [
-  -1, -1, 1, -1, -1,
-  -1, 1, 1, -1, -1,
-  1, -1, 1, -1, -1,
-  -1, -1, 1, -1, -1,
-  -1, 1, 1, 1, -1
-]
-
-# Número 2
-numero_dois = [
-  -1, 1, 1, -1, -1,
-  1, -1, -1, 1, -1,
-  -1, -1, 1, -1, -1,
-  -1, 1, -1, -1, -1,
-  1, 1, 1, 1, -1
-]
-
-# Número 3
-numero_tres = [
-  -1, 1, 1, -1, -1,
-  1, -1, -1, 1, -1,
-  -1, -1, 1, -1, -1,
-  1, -1, -1, 1, -1,
-  -1, 1, 1, -1, -1
-]
-
-# Número 4
-numero_quatro = [
-  1, -1, 1, -1, -1,
-  1, -1, 1, -1, -1,
-  1, 1, 1, 1, 1,
-  -1, -1, 1, -1, -1,
-  -1, -1, 1, -1, -1
-]
-
-# Número 5
-numero_cinco = [
-  1, 1, 1, -1, -1,
-  1, -1, -1, -1, -1,
-  -1, 1, 1, -1, -1,
-  -1, -1, -1, 1, -1,
-  1, 1, 1, -1, -1
-]
-
-# Número 6
-numero_seis = [
-  -1, 1, 1, -1, -1,
-  1, -1, -1, -1, -1,
-  1, 1, 1, -1, -1,
-  1, -1, -1, 1, -1,
-  -1, 1, 1, -1, -1
-]
-
-# Número 7
-
-numero_sete = [
-  1, 1, 1, 1, 1,
-  -1, -1, -1, 1, -1,
-  -1, -1, 1, -1, -1,
-  -1, 1, -1, -1, -1,
-  -1, 1, -1, -1, -1
-]
-numero_oito = [
--1, 1, 1, -1, -1,
-1, -1, -1, 1, -1,
--1, 1, 1, -1, -1,
-1, -1, -1, 1, -1,
--1, 1, 1, -1, -1
-]
-
-numero_nove = [
--1, 1, 1, -1, -1,
-1, -1, -1, 1, -1,
--1, 1, 1, 1, -1,
--1, -1, -1, 1, -1,
--1, 1, 1, -1, -1
-]
-
-NUMBERS = [numero_zero, numero_um, numero_dois, numero_tres, numero_quatro, numero_cinco, numero_seis, numero_sete, numero_oito, numero_nove]
-
-# Número -1
-numero_zero_alt = [
-  1, 1, 1, 1, 1,
-  1, -1, -1, -1, 1,
-  1, -1, -1, -1, 1,
-  1, -1, -1, -1, 1,
-  1, 1, 1, 1, 1
-]
-
-# Número 1
-numero_um_alt = [
-  -1, -1, 1, -1, -1,
-  -1, 1, 1, -1, -1,
-  -1, -1, 1, -1, -1,
-  -1, -1, 1, -1, -1,
-  -1, 1, 1, 1, -1
-]
-
-# Número 2
-numero_dois_alt = [
-  -1, 1, 1, -1, -1,
-  1, -1, -1, 1, -1,
-  -1, -1, 1, -1, -1,
-  -1, 1, -1, -1, -1,
-  1, 1, 1, 1, 1
-]
-
-# Número 3
-numero_tres_alt = [
-  1, 1, 1, -1, -1,
-  -1, -1, -1, 1, -1,
-  -1, -1, 1, -1, -1,
-  -1, -1, -1, 1, -1,
-  1, 1, 1, -1, -1
-]
-
-# Número 4
-numero_quatro_alt = [
-  1, -1, 1, -1, -1,
-  1, -1, 1, -1, -1,
-  1, 1, 1, 1, 1,
-  -1, -1, 1, -1, -1,
-  -1, -1, 1, -1, -1
-]
-
-# Número 5
-numero_cinco_alt = [
-  1, 1, 1, 1, -1,
-  1, -1, -1, -1, -1,
-  1, 1, 1, -1, -1,
-  -1, -1, -1, 1, -1,
-  1, 1, 1, -1, -1
-]
-
-# Número 6
-numero_seis_alt = [
-  -1, 1, 1, -1, -1,
-  1, -1, -1, -1, -1,
-  1, 1, 1, -1, -1,
-  1, -1, -1, 1, -1,
-  -1, 1, 1, -1, -1
-]
-
-
-# Número 7
-numero_sete_alt = [
-  1, 1, 1, 1, 1,
-  -1, -1, -1, -1, 1,
-  -1, -1, -1, 1, -1,
-  -1, -1, 1, -1, -1,
-  -1, 1, -1, -1, -1
-]
-
-# Número 8
-numero_oito_alt = [
-  -1, 1, 1, -1, -1,
-  1, -1, -1, 1, -1,
-  -1, 1, 1, -1, -1,
-  1, -1, -1, 1, -1,
-  -1, 1, 1, -1, -1
-]
-
-# Número 9
-numero_nove_alt = [
-  -1, 1, 1, -1, -1,
-  1, -1, -1, 1, -1,
-  -1, 1, 1, 1, -1,
-  -1, -1, -1, 1, -1,
-  -1, 1, 1, -1, -1
-]
-
-NUMBERS_ALT = [numero_zero_alt, numero_um_alt, numero_dois_alt, numero_tres_alt, numero_quatro_alt, numero_cinco_alt, numero_seis_alt, numero_sete_alt, numero_oito_alt, numero_nove_alt]
-
-
 from model import Perceptron
+from digits import *
+
 
 def get_data():
-
     outputs = [0,1,2,3,4,5,6,7,8,9] 
-
-
     inputs = NUMBERS
-
 
     return inputs, outputs
 
 
+def get_data_2():
+    outputs = [0,1,2,3,4,5,6,7,8,9] * 2
+    inputs = NUMBERS + NUMBERS_ALT
+
+    return inputs, outputs
+
+# Treino com uma variação de numeros
 inputs, outputs = get_data()
 
 ia = Perceptron()
 
 ia.fit(inputs, outputs)
 
-def get_data_2():
 
-    outputs = [0,1,2,3,4,5,6,7,8,9] * 2
-
-
-    inputs = NUMBERS + NUMBERS_ALT
-
-
-    return inputs, outputs
-
-
+# Treino com duas variações de numeros
 inputs2, outputs2 = get_data_2()
 
 ia2 = Perceptron()
