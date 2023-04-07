37b14bcbbb03ad38c26be01fd1c113e490077f00
gbPagano <guilhermebpagano@gmail.com>
2023-04-06 21:13:09 -0300

    trabalho05

--- trabalho_05/main.py
+++ trabalho_05/main.py
@@ -1,32 +1,221 @@
-import pandas as pd
+# Número -1
+numero_zero = [
+  -1, 1, 1, 1, -1,
+  1, -1, -1, -1, 1,
+  1, -1, -1, -1, 1,
+  1, -1, -1, -1, 1,
+  -1, 1, 1, 1, -1
+]
 
+# Número 1
+numero_um = [
+  -1, -1, 1, -1, -1,
+  -1, 1, 1, -1, -1,
+  1, -1, 1, -1, -1,
+  -1, -1, 1, -1, -1,
+  -1, 1, 1, 1, -1
+]
+
+# Número 2
+numero_dois = [
+  -1, 1, 1, -1, -1,
+  1, -1, -1, 1, -1,
+  -1, -1, 1, -1, -1,
+  -1, 1, -1, -1, -1,
+  1, 1, 1, 1, -1
+]
+
+# Número 3
+numero_tres = [
+  -1, 1, 1, -1, -1,
+  1, -1, -1, 1, -1,
+  -1, -1, 1, -1, -1,
+  1, -1, -1, 1, -1,
+  -1, 1, 1, -1, -1
+]
+
+# Número 4
+numero_quatro = [
+  1, -1, 1, -1, -1,
+  1, -1, 1, -1, -1,
+  1, 1, 1, 1, 1,
+  -1, -1, 1, -1, -1,
+  -1, -1, 1, -1, -1
+]
+
+# Número 5
+numero_cinco = [
+  1, 1, 1, -1, -1,
+  1, -1, -1, -1, -1,
+  -1, 1, 1, -1, -1,
+  -1, -1, -1, 1, -1,
+  1, 1, 1, -1, -1
+]
+
+# Número 6
+numero_seis = [
+  -1, 1, 1, -1, -1,
+  1, -1, -1, -1, -1,
+  1, 1, 1, -1, -1,
+  1, -1, -1, 1, -1,
+  -1, 1, 1, -1, -1
+]
+
+# Número 7
+
+numero_sete = [
+  1, 1, 1, 1, 1,
+  -1, -1, -1, 1, -1,
+  -1, -1, 1, -1, -1,
+  -1, 1, -1, -1, -1,
+  -1, 1, -1, -1, -1
+]
+numero_oito = [
+-1, 1, 1, -1, -1,
+1, -1, -1, 1, -1,
+-1, 1, 1, -1, -1,
+1, -1, -1, 1, -1,
+-1, 1, 1, -1, -1
+]
+
+numero_nove = [
+-1, 1, 1, -1, -1,
+1, -1, -1, 1, -1,
+-1, 1, 1, 1, -1,
+-1, -1, -1, 1, -1,
+-1, 1, 1, -1, -1
+]
+
+NUMBERS = [numero_zero, numero_um, numero_dois, numero_tres, numero_quatro, numero_cinco, numero_seis, numero_sete, numero_oito, numero_nove]
+
+# Número -1
+numero_zero_alt = [
+  1, 1, 1, 1, 1,
+  1, -1, -1, -1, 1,
+  1, -1, -1, -1, 1,
+  1, -1, -1, -1, 1,
+  1, 1, 1, 1, 1
+]
+
+# Número 1
+numero_um_alt = [
+  -1, -1, 1, -1, -1,
+  -1, 1, 1, -1, -1,
+  -1, -1, 1, -1, -1,
+  -1, -1, 1, -1, -1,
+  -1, 1, 1, 1, -1
+]
+
+# Número 2
+numero_dois_alt = [
+  -1, 1, 1, -1, -1,
+  1, -1, -1, 1, -1,
+  -1, -1, 1, -1, -1,
+  -1, 1, -1, -1, -1,
+  1, 1, 1, 1, 1
+]
+
+# Número 3
+numero_tres_alt = [
+  1, 1, 1, -1, -1,
+  -1, -1, -1, 1, -1,
+  -1, -1, 1, -1, -1,
+  -1, -1, -1, 1, -1,
+  1, 1, 1, -1, -1
+]
+
+# Número 4
+numero_quatro_alt = [
+  1, -1, 1, -1, -1,
+  1, -1, 1, -1, -1,
+  1, 1, 1, 1, 1,
+  -1, -1, 1, -1, -1,
+  -1, -1, 1, -1, -1
+]
+
+# Número 5
+numero_cinco_alt = [
+  1, 1, 1, 1, -1,
+  1, -1, -1, -1, -1,
+  1, 1, 1, -1, -1,
+  -1, -1, -1, 1, -1,
+  1, 1, 1, -1, -1
+]
+
+# Número 6
+numero_seis_alt = [
+  -1, 1, 1, -1, -1,
+  1, -1, -1, -1, -1,
+  1, 1, 1, -1, -1,
+  1, -1, -1, 1, -1,
+  -1, 1, 1, -1, -1
+]
+
+
+# Número 7
+numero_sete_alt = [
+  1, 1, 1, 1, 1,
+  -1, -1, -1, -1, 1,
+  -1, -1, -1, 1, -1,
+  -1, -1, 1, -1, -1,
+  -1, 1, -1, -1, -1
+]
+
+# Número 8
+numero_oito_alt = [
+  -1, 1, 1, -1, -1,
+  1, -1, -1, 1, -1,
+  -1, 1, 1, -1, -1,
+  1, -1, -1, 1, -1,
+  -1, 1, 1, -1, -1
+]
+
+# Número 9
+numero_nove_alt = [
+  -1, 1, 1, -1, -1,
+  1, -1, -1, 1, -1,
+  -1, 1, 1, 1, -1,
+  -1, -1, -1, 1, -1,
+  -1, 1, 1, -1, -1
+]
+
+NUMBERS_ALT = [numero_zero_alt, numero_um_alt, numero_dois_alt, numero_tres_alt, numero_quatro_alt, numero_cinco_alt, numero_seis_alt, numero_sete_alt, numero_oito_alt, numero_nove_alt]
+
+
+from model import Perceptron
 
 def get_data():
-    data = pd.read_csv("train.csv")
 
-    outputs = list(data.label)
-    raw_inputs = data.drop("label", axis="columns")
+    outputs = [0,1,2,3,4,5,6,7,8,9] 
 
-    inputs = [[-1 if not x else 1 for x in line] for _, line in raw_inputs.iterrows()]
+
+    inputs = NUMBERS
+
 
     return inputs, outputs
 
-import polars as pl
 
+inputs, outputs = get_data()
+
+ia = Perceptron()
+
+ia.fit(inputs, outputs)
 
 def get_data_2():
-    data = pl.read_csv("train.csv")
 
-    outputs = list(data.drop_in_place("label")) 
-    inputs = [[-1 if not x else 1 for x in line] for line in data.rows()]
+    outputs = [0,1,2,3,4,5,6,7,8,9] * 2
+
+
+    inputs = NUMBERS + NUMBERS_ALT
+
 
     return inputs, outputs
 
 
+inputs2, outputs2 = get_data_2()
 
-from timeit import timeit
+ia2 = Perceptron()
 
-print("pandas:", timeit('get_data()', 'from __main__ import get_data', number=3))
-print("polars:", timeit('get_data_2()', 'from __main__ import get_data_2', number=3))
+ia2.fit(inputs2, outputs2)
 
 
--- trabalho_05/model.py
+++ trabalho_05/model.py
@@ -5,13 +5,15 @@ class Perceptron:
         self.alpha = alpha
 
 
-    def fit(self, inputs, real_outputs):
+    def fit(self, inputs, raw_outputs):
+        self.iterations = 0
         self.inputs = inputs
-        self.outputs = real_outputs
+        self.outputs = raw_outputs
 
-        for idx in range(len(self.inputs)):
+        for i in range(len(self.inputs)):
             outputs = [-1] * 9
-            outputs.insert(idx, self.outputs[idx])
+            idx = self.outputs[i]
+            outputs.insert(idx, 1)
 
             result = self._train_neuron(outputs)
             self.weights.append(result)
@@ -34,52 +36,61 @@ class Perceptron:
                 output = -1
 
             result.append(output)
-        
-        idx = result.index(1)
-        return "Failed" if not idx else idx
-
+        try:
+            return result.index(1)
+        except ValueError:
+            return "Failed"
 
 
     def _validate_perceptron(self):
+        self.acertos = 0
+        self.total= 0
         for idx, (weights, bias) in enumerate(self.weights):
             outputs = [-1] * len(self.inputs)
             outputs.insert(idx, 1)
 
-            if not self._validate_neuron(outputs, weights, bias):
-                raise Exception("It was not possible to train the neuron, possibly it is not a linearly solvable problem")
+            self._validate_neuron(outputs, weights, bias)
+        
+        result = self.acertos/self.total
+
+        print(f"Trained with {result*100}% of precision")
 
     def _validate_neuron(self, real_outputs, weights, bias):
         for i, vetor in enumerate(self.inputs):
+            self.total += 1
             pre_output = bias
-        
-            for idx, item in enumerate(vetor):
-                pre_output += item * weights[idx]
+            idx = self.outputs[i]
+
+            for j, item in enumerate(vetor):
+                pre_output += item * weights[j]
 
             if pre_output >= 0:
                 output = 1
             else:
                 output = -1
 
-            if output != real_outputs[i]:
-                return False
+            if output == real_outputs[idx]:
+                self.acertos += 1
 
-        return True
 
 
     def _train_neuron(self, real_outputs):
         weights = [0] * len(self.inputs[0])
         bias = 0
-
+        
         training = True
         while training:
+            print("iteration:", self.iterations)
+            self.iterations += 1
             training = False
             for i, input in enumerate(self.inputs):
-                error = self._calc_error(input, real_outputs[i], weights, bias)
+                idx = self.outputs[i]
+                error = self._calc_error(input, real_outputs[idx], weights, bias)
                 if error:
                     for j, item in enumerate(input):
                         training = True
-                        weights[j] += item * real_outputs[i] * self.alpha
-                    bias += real_outputs[i] * self.alpha
+                        weights[j] += item * real_outputs[idx] * self.alpha
+                    bias += real_outputs[idx] * self.alpha
        
         return weights, bias
 
@@ -94,8 +105,7 @@ class Perceptron:
         else:
             output = -1
 
-        error = real_output - output
-        return error
+        return real_output != output
 
 
 
