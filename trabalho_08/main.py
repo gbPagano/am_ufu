f7dafb484a72586a2c39724bb6c17dac5b40101e
gbPagano <guilhermebpagano@gmail.com>
2023-05-05 22:38:08 -0300

    implementing moduled forwarding layers

--- trabalho_08/main.py
+++ trabalho_08/main.py
@@ -1,69 +1,68 @@
 import numpy as np
 
 
+class Tanh:
+    def activate(self, x):
+        return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+    
+    def derivative(self, x):
+        return 1 - self.activate(x) ** 2
 
-x_train = np.array([0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1.])
-y_train = [-.9602, -.5770, -.0729,  .3771,  .6405,  .6600,  .4609, .1336, -.2013, -.4344, -.5000 ];
-x_train = np.array([np.array([1, x]) for x in x_train])
-y_train = np.array([np.array([y]) for y in y_train])
 
+class Layer:
+    def __init__(self, neurons, len_inputs, function):
+        shape = neurons, len_inputs
+        self.weights = np.random.uniform(-0.5, 0.5, size=shape)
+        self.f = function
+    
+    def forward(self, layer_input):
+        self.input = layer_input
+        self.net = self.input.dot(self.weights.T)
+        self.output = self.f.activate(self.net)
+        return self.output
 
 
 class NeuralNetwork:
-    def __init__(self, x_train, y_train, alpha=0.01):
-        self.alpha = alpha
-        self.x = x_train
-        self.d = y_train
-        self.weights_1 = np.random.randn(2, len(x_train[0])) # first layer
-        self.weights_2 = np.random.randn(1, len(self.weights_1[0]) + 1) # second layer
-    
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-
-    def sigmoid_derivative(self, x):
-        return self.sigmoid(x) * (1 - self.sigmoid(x))
-
-    def fit(self, epochs=500):
-
-        for _ in range(epochs):
-            # feedfowarding
-            net_1 = np.dot(self.x, self.weights_1.T)
-            output_1 = [self.sigmoid(u) for u in net_1]
-            input_2 = np.array([np.append(1, u) for u in output_1])
-            net_2 = np.dot(input_2, self.weights_2.T)
-            output_2 = [self.sigmoid(u) for u in net_2]
-
-            # backpropagation
-            delta_2 = (self.d - output_2) * self.sigmoid_derivative(net_2)
-            self.weights_2 += sum(self.alpha * delta_2 * input_2)
-            
-            delta_1 = [[-sum(x)] for x in delta_2 * self.weights_2] * self.sigmoid_derivative(net_1)
-            self.weights_1 += sum(self.alpha * delta_1 * self.x)
-
+    def __init__(self, *layers):
+        self.layers = list(layers)
         
-    def predict(self, x):
-        net_1 = np.dot(x, self.weights_1.T)
-        output_1 = [self.sigmoid(u) for u in net_1]
-        input_2 = np.array([np.append(1, u) for u in output_1])
-        net_2 = np.dot(input_2, self.weights_2.T)
-        output_2 = [self.sigmoid(u) for u in net_2]
-
-        return output_2
-
-
-
-    
+    def forward(self, x_input):
+        #input_layer = x_input
+        input_layer = np.array([np.append(-1, x) for x in x_input])
+        for layer in self.layers:
+            out_layer = layer.forward(input_layer)
+            input_layer = np.array([np.append(-1, x) for x in out_layer])
+            
+        return out_layer
 
 
-model = NeuralNetwork(x_train, y_train)
+weights_1 = np.array([
+    [0.2, .4, .5],
+    [.3, .6, .7],
+    [.4, .8, .3],
+])
+weights_2 = np.array([
+    [-.7, .6, .2, .7],
+    [-.3, .7, .2, .8],
+])
+weights_3 = np.array([
+    [.1, .8, .5],
+])
+
+x_input = np.array([
+    [0.3, 0.7],
+    [0.6, 1.4],
+])
 
 
+rede = NeuralNetwork(
+    Layer(neurons=3, len_inputs=3, function=Tanh()),
+    Layer(neurons=2, len_inputs=4, function=Tanh()),
+    Layer(neurons=1, len_inputs=3, function=Tanh()),
+)
+rede.layers[0].weights = weights_1
+rede.layers[1].weights = weights_2
+rede.layers[2].weights = weights_3
 
-
-
-
-
-
-
-
-
+res = rede.forward(x_input)
+print(res)
