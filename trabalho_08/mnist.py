132071df18947523dfed98d0881022b78b8d3e0f
gbPagano <guilhermebpagano@gmail.com>
2023-05-09 22:28:16 -0300

    saving weights and testing batch train

--- trabalho_08/mnist.py
+++ trabalho_08/mnist.py
@@ -1,18 +1,13 @@
-
 import numpy as np
-from numba import njit, jit, experimental
+from rich.progress import track
 
-class Tanh: 
+class Tanh:
     def activate(self, x):
-        return self.f(x)
-
-    @staticmethod
-    @jit(nopython=True)
-    def f(x):
         return np.tanh(x)
- 
+    
     def derivative(self, x):
         return 1 - self.activate(x) ** 2
+
 
 class Layer:
     def __init__(self, len_inputs, neurons, function, last=False):
@@ -23,21 +18,20 @@ class Layer:
         self.idx = None
         self.neurons = neurons
         self.len_inputs = len_inputs
-
+    
     def forward(self, layer_input):
         self.input = layer_input
         self.net = self.input.dot(self.weights.T)
         self.output = self.f.activate(self.net)
         return self.output
-
-    def backward(self, target, alpha, previous_delta=None, previous_weigth=None):
+    
+    def backward(self, alpha, previous_delta=None, previous_weigth=None, error=None):
         if self.last:
-            self.delta = (target - self.output) * self.f.derivative(self.net)
+            self.delta = error * self.f.derivative(self.net)
         else:
-            #self.delta = (np.delete(previous_delta.dot(previous_weigth).T, 0) * self.f.derivative(self.net))
-            self.delta = (np.delete(previous_weigth.T.dot(previous_delta), 0) * self.f.derivative(self.net))
-            assert previous_weigth.T.dot(previous_delta).all() == previous_delta.dot(previous_weigth).T.all()
-        self.weights += self.delta.T * self.input * alpha
+            self.delta = (np.delete(previous_delta.dot(previous_weigth).T, 0) * self.f.derivative(self.net))
+        
+        self.weights += np.array([self.delta]).T * np.array([self.input]) * alpha
         
         return self.delta, self.weights
         
@@ -68,58 +62,66 @@ class NeuralNetwork:
         
     def _forward(self, x_input):
         #input_layer = x_input
-        input_layer = np.array([np.append(1, x) for x in x_input])
+        input_layer = np.append(1, x_input)
         for layer in self.layers:
             out_layer = layer.forward(input_layer)
-            input_layer = np.array([np.append(1, x) for x in out_layer])
+            input_layer = np.append(1, out_layer)
             
         return out_layer
     
-    def _backward(self, y, alpha):
+    def _backward(self, alpha, error):
         for layer in reversed(self.layers):
             if layer.last:
-                previous_delta, previous_weigth = layer.backward(y, alpha)
+                previous_delta, previous_weigth = layer.backward(alpha, error=error)
             else:
-                previous_delta, previous_weigth = layer.backward(y, alpha, previous_delta, previous_weigth)
+                previous_delta, previous_weigth = layer.backward(alpha, previous_delta, previous_weigth)
     
-    def fit(self, x_train, y_train, epochs=2000, alpha=0.05):
-        for epoch in range(epochs):
+    def fit(self, x_train, y_train, epochs=2000, alpha=0.05, batch_size=1):
+
+        for epoch in track(range(epochs), description="Processing..."):
+            outputs = []
+            batch_errors = []
+            data = list(zip(x_train,y_train))
+            np.random.shuffle(data)
+            x_train,y_train = zip(*data)
+            x_train,y_train = np.array(x_train), np.array(y_train)
             for x, y in zip(x_train, y_train):
-                self._forward(x)
-                self._backward(y, alpha)
+                out = self._forward(x)
+                error = (y - out)
+
+                batch_errors.append(error)
+                if len(batch_errors) == batch_size:
                 
-            out = self._forward(x_train)
-            errors = np.array([sum(error) for error in (y_train - out) ** 2])
+                    batch_error = sum(batch_errors) / batch_size
+                    batch_errors = []
+                    self._backward(alpha, batch_error)
+
+                outputs.append(out)
+                
+            errors = np.array([sum(error) for error in (y_train - outputs) ** 2])
             self.mean_squared_error = sum(errors) / len(errors)
             
             if not epoch % 100:
                 print(f"MSE: {self.mean_squared_error}")
+
                 
                 
     def predict(self, x):
         out = self._forward(x)
         return out
+    
+
+import polars as pl
 
 
+def number_to_neurons(n):
+    res = [-1] * 10
+    res[n] = 1
+    return res
 
+data_train = pl.read_csv("train.csv")
 
-x_train = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1.0]
-y_train = [-.9602, -.5770, -.0729, .3771, .6405, .6600, .4609, .1336, -.2013, -.4344, -.5000 ]
-x_train = np.array([[x] for x in x_train])
-y_train = np.array([[y] for y in y_train])
+y_train = np.array(data_train.drop_in_place("label"))
+y_train = np.array([number_to_neurons(y) for y in y_train])
 
-
-rede = NeuralNetwork(
-    Layer(len_inputs=1, neurons=4, function=Tanh()),
-    Layer(len_inputs=4, neurons=1, function=Tanh()),
-)
-rede.fit(x_train, y_train)
-out = rede.predict(x_train)
-
-
-import matplotlib.pyplot as plt
-
-
-plt.scatter(x_train, y_train, color="green")
-plt.scatter(x_train, out, color="red")
-plt.show()
+x_train = np.array([row for row in data_train.rows()]) / 255
