b3b62c9db6c067a0c3914c247fe392bb53e92d55
gbPagano <guilhermebpagano@gmail.com>
2023-06-01 10:01:01 -0300

    task11 finalized

--- trabalho_11/main.py
+++ trabalho_11/main.py
@@ -1,15 +1,14 @@
-import matplotlib.pyplot as plt
-import random
-import numpy as np
 import math
-from rich.traceback import install
-install()
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class KPoint:
     def __init__(
-        self, 
-        interval_x: tuple[float, float], 
+        self,
+        interval_x: tuple[float, float],
         interval_y: tuple[float, float],
     ):
         x = random.uniform(interval_x[0], interval_x[1])
@@ -21,23 +20,17 @@ class KPoint:
     def update_center(self):
         if self.old_points == self.points:
             self.points = []
-            return False 
+            return False
 
-        if self.points: 
+        if self.points:
             self.k_center = np.mean(self.points, axis=0)
-        
-        self.old_points = self.points.copy()
+            self.old_points = self.points.copy()
+
         self.points = []
         return True
 
-
     def __repr__(self):
         return f"{self.k_center}"
-
-    
-
-
-
 
 
 def main():
@@ -46,30 +39,31 @@ def main():
         points = [list(map(float, data.strip().split())) for data in lines]
 
     x, y = zip(*points)
-    
+
     results = k_means(
-        k=4, 
-        points=points, 
-        interval_x=(min(x), max(x)), 
-        interval_y=(min(y), max(y)), 
+        k=4,
+        points=points,
+        interval_x=(min(x), max(x)),
+        interval_y=(min(y), max(y)),
     )
-    
+
     plt.scatter(x, y, color="red")
-    print(results) 
+    print(results)
     for item in results:
-        x, y = item
-        plt.plot(x, y, "bo")
+        x, y = item.k_center
+        a, b = zip(*item.old_points)
+        plt.scatter(a, b)
+        plt.scatter(x, y)
 
     plt.show()
 
 
 def k_means(
-    k: int, 
-    points: list[list[float]], 
-    interval_x: tuple[float, float], 
+    k: int,
+    points: list[list[float]],
+    interval_x: tuple[float, float],
     interval_y: tuple[float, float],
 ):
-    
     k_points = []
 
     for _ in range(k):
@@ -77,21 +71,18 @@ def k_means(
         k_points.append(k_point)
 
     results = [True]
-    while any(results): 
+    while any(results):
         for point in points:
-            # import ipdb; ipdb.set_trace()
-            distances = [(math.dist(point, k_point.k_center), idx) for (idx, k_point) in enumerate(k_points)]
+            distances = [
+                (math.dist(point, k_point.k_center), idx)
+                for (idx, k_point) in enumerate(k_points)
+            ]
             idx = min(distances)[1]
             k_points[idx].points.append(point)
 
-
         results = [k_point.update_center() for k_point in k_points]
-        print(results)
-        
 
-    return [k_point.k_center for k_point in k_points]
-    
-
+    return [k_point for k_point in k_points]
 
 
 if __name__ == "__main__":
